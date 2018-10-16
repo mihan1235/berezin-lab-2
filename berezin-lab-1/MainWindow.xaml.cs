2b523d6563e6a4bc42d0b45005ec82fc19b44964
mihan1235 <mihan1235@yandex.ru>
2018-10-16 14:09:52 +0300

    app opens image can save it with changes made to canvas. Used viewbox for image and canvas because the coordinates are changed with the changed size of grid. In this grid there is image and canvas. With canvas we can add rectangles and they can be saved on image.

--- berezin-lab-1/MainWindow.xaml.cs
+++ berezin-lab-1/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,5 +26,57 @@ namespace berezin_lab_1
         {
             InitializeComponent();
         }
+
+        private void CanSaveImage(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void SaveImage(object sender, ExecutedRoutedEventArgs e)
+        {
+            
+            SaveFileDialog save_diag = new SaveFileDialog();
+            if (save_diag.ShowDialog() == true)
+            {
+                try
+                {
+                    var bmp = new RenderTargetBitmap(
+                        (int)cs.ActualWidth, (int)cs.ActualHeight, 96, 96, PixelFormats.Default);
+                    bmp.Render(cs);
+                    var enc = new PngBitmapEncoder();
+                    enc.Frames.Add(BitmapFrame.Create(bmp));
+                    using (var s = File.Create(save_diag.FileName))
+                        enc.Save(s);
+                }
+                catch (Exception exc)
+                {
+                    MessageBox.Show(exc.ToString(), "Exception", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+
+            }
+        }
+
+        private void CanOpenImage(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void OpenImage(object sender, ExecutedRoutedEventArgs e)
+        {
+            OpenFileDialog open_diag = new OpenFileDialog();
+            if (open_diag.ShowDialog() == true)
+            {
+                try
+                {
+                    var img = new BitmapImage(new Uri(open_diag.FileName, UriKind.RelativeOrAbsolute));
+                    ImageObject.Source = img;
+                }
+                catch (Exception exc)
+                {
+                    MessageBox.Show(exc.ToString(), "Exception", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+
     }
 }
